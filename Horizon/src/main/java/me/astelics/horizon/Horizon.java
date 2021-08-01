79ac63c63c9c72c3fb8d8b8329f36322824f2e84
AsteIics <81925127+AsteIics@users.noreply.github.com>
2021-08-01 11:47:55 +0100

    optimized loading
    optimized command and listener loading + added new arraylist

--- Horizon/src/main/java/me/astelics/horizon/Horizon.java
+++ Horizon/src/main/java/me/astelics/horizon/Horizon.java
@@ -1,6 +1,12 @@
 package me.astelics.horizon;
 
-import me.astelics.horizon.commands.*;
+import me.astelics.horizon.commands.essentials.FlyCommand;
+import me.astelics.horizon.commands.essentials.HealCommand;
+import me.astelics.horizon.commands.essentials.KillCommand;
+import me.astelics.horizon.commands.essentials.MessageCommand;
+import me.astelics.horizon.commands.punishment.KickCommand;
+import me.astelics.horizon.commands.random.BigMeCommand;
+import me.astelics.horizon.commands.staff.IncognitoCommand;
 import me.astelics.horizon.listeners.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -8,32 +14,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 
-public final class Horizon extends JavaPlugin implements Listener
-{
+public final class Horizon extends JavaPlugin implements Listener {
     public ArrayList<Player> vanishedPlayers = new ArrayList<>();
+    public ArrayList<Player> bannedPlayers = new ArrayList<>();
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         // Plugin startup logic
-        System.out.println("[Horizon] Loaded plugin successfully.");
+        loadCommands();
+        loadListeners();
 
-        getServer().getPluginManager().registerEvents(new XPBottleBreak(), this);
-        getServer().getPluginManager().registerEvents(new ShearSheepListener(), this);
-        getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
-        getServer().getPluginManager().registerEvents(new BigMeListener(), this);
-        getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
+        System.out.println("[Horizon] Loaded plugin successfully.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("[Horizon] Plugin shut down.");
+    }
+
+    private void loadCommands() {
         getCommand("incognito").setExecutor(new IncognitoCommand(this));
+        getCommand("message").setExecutor(new MessageCommand());
+        getCommand("kick").setExecutor(new KickCommand());
         getCommand("kill").setExecutor(new KillCommand());
         getCommand("heal").setExecutor(new HealCommand());
         getCommand("fly").setExecutor(new FlyCommand());
         getCommand("bigme").setExecutor(new BigMeCommand());
     }
 
-    @Override
-    public void onDisable()
-    {
-        // Plugin shutdown logic
-        System.out.println("[Horizon] Plugin shut down.");
+    private void loadListeners() {
+        getServer().getPluginManager().registerEvents(new XPBottleBreak(), this);
+        getServer().getPluginManager().registerEvents(new ShearSheepListener(), this);
+        getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
+        getServer().getPluginManager().registerEvents(new BigMeListener(), this);
+        getServer().getPluginManager().registerEvents(new IncognitoListener(this), this);
     }
 }
